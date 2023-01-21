0ec9fbdd961237e27ffe611e7378bf8fabdacbd7
lakshmanangithubonee <lakshman15cse@gmail.com>
2023-01-21 14:47:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
 
 function App() {
+  // console.log(import.meta.env.Open_AI_Key);
+  // const OPENAI_API_KEY = `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`;
+  // const configuration = new Configuration({
+  //   apiKey: process.env.OPENAI_API_KEY,
+  // });
+
+  // const openai = new OpenAIApi(configuration);
+
+  // const generateImage = async () => {
+  //   const res = await openai.createImage({
+  //     prompt: prompt,
+  //     n: 1,
+  //     size: "512x512",
+  //   });
+  //   console.log(res.data);
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button onClick={generateImage}>generate an image</button> */}
     </div>
   );
 }
